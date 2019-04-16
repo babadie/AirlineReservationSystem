27fcc7b4840a10906d7b01c1b46cf5759ea36e07
benitoabadie <benitoabadie@10.250.18.173>
2019-04-15 20:15:26 -0400

    refresh

---
+++ AirlineRevervations/src/BusinessLogic/BizLogic.java
@@ -0,0 +1,10 @@
+package BusinessLogic;
+
+public class BizLogic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AirlineRevervations/src/DatabaseOracle/Database.java
@@ -0,0 +1,10 @@
+package DatabaseOracle;
+
+public class Database {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
