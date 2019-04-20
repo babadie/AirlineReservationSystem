a269274b6ed1256486961125eba0b7d5e4b3742c
makaveliohaya <makaveliohaya@makavelis-air.wifi.gsu.edu>
2019-04-20 14:30:32 -0400

    cities addded

--- AirlineRevervations/src/BusinessLogic/Cities.java
+++ AirlineRevervations/src/BusinessLogic/Cities.java
@@ -2,9 +2,203 @@ package BusinessLogic;
 
 public class Cities {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		private String cityName;
+		private String Location;
+		private String cityId;
+		private String cityName2;
+		private String Location2;
+		private String cityId2;
+		private String cityName3;
+		private String Location3;
+		private String cityId3;
+		private String cityName4;
+		private String Location4;
+		private String cityId4;
+		private String cityName5;
+		private String Location5;
+		private String cityId5;
+		private String cityName6;
+		private String Location6;
+		private String cityId6;
 
-	}
 
-}
+			public Cities(){
+			cityName = "Atlanta";
+			Location = "Georgia";
+			cityId = "ATL";
+			cityName2 = "Los Angeles";
+			Location2 = "California";
+			cityId2 = "LA";
+			cityName3 = "Orlando";
+			Location3 = "Florida";
+			cityId3 = "FL";
+			cityName4 = "Houston";
+			Location4 ="Texas";
+			cityId4 = "TX";
+			cityName5 = "Montogomery";
+			Location5 = "Alabama";
+			cityId5 = "Al";
+			cityName6 = "New York City";
+			Location6 = "New York";
+			cityId6 = "NY";
+			}
+			
+			
+			
+			public String getCityName() {
+				return cityName;
+			}
+
+			public void setCityName(String cityName) {
+				this.cityName = cityName;
+				
+			}
+
+			public String getLocation() {
+				return Location;
+			}
+
+			public void setLocation(String location) {
+				Location = location;
+			}
+
+			public String getCityId() {
+				return cityId;
+			}
+
+			public void setCityId(String cityId) {
+				this.cityId = cityId;
+			}
+			
+			public String getCityName2() {
+				return cityName2;
+			}
+
+			public String getLocation2() {
+				return Location2;
+			}
+
+			public String getCityId2() {
+				return cityId2;
+			}
+
+			public String getCityName3() {
+				return cityName3;
+			}
+
+			public String getLocation3() {
+				return Location3;
+			}
+
+			public String getCityId3() {
+				return cityId3;
+			}
+
+			public String getCityName4() {
+				return cityName4;
+			}
+
+			public String getLocation4() {
+				return Location4;
+			}
+
+			public String getCityId4() {
+				return cityId4;
+			}
+
+			public String getCityName5() {
+				return cityName5;
+			}
+
+			public String getLocation5() {
+				return Location5;
+			}
+
+			public String getCityId5() {
+				return cityId5;
+			}
+
+			public String getCityName6() {
+				return cityName6;
+			}
+
+			public String getLocation6() {
+				return Location6;
+			}
+
+			public String getCityId6() {
+				return cityId6;
+			}
+
+			public void setCityName2(String cityName2) {
+				this.cityName2 = cityName2;
+			}
+
+			public void setLocation2(String location2) {
+				Location2 = location2;
+			}
+
+			public void setCityId2(String cityId2) {
+				this.cityId2 = cityId2;
+			}
+
+			public void setCityName3(String cityName3) {
+				this.cityName3 = cityName3;
+			}
+
+			public void setLocation3(String location3) {
+				Location3 = location3;
+			}
+
+			public void setCityId3(String cityId3) {
+				this.cityId3 = cityId3;
+			}
+
+			public void setCityName4(String cityName4) {
+				this.cityName4 = cityName4;
+			}
+
+			public void setLocation4(String location4) {
+				Location4 = location4;
+			}
+
+			public void setCityId4(String cityId4) {
+				this.cityId4 = cityId4;
+			}
+
+			public void setCityName5(String cityName5) {
+				this.cityName5 = cityName5;
+			}
+
+			public void setLocation5(String location5) {
+				Location5 = location5;
+			}
+
+			public void setCityId5(String cityId5) {
+				this.cityId5 = cityId5;
+			}
+
+			public void setCityName6(String cityName6) {
+				this.cityName6 = cityName6;
+			}
+
+			public void setLocation6(String location6) {
+				Location6 = location6;
+			}
+
+			public void setCityId6(String cityId6) {
+				this.cityId6 = cityId6;
+			}
+
+			@Override
+			public String toString() {
+				return "Cities [cityName=" + cityName + ", Location=" + Location + ", cityId=" + cityId + ", cityName2="
+						+ cityName2 + ", Location2=" + Location2 + ", cityId2=" + cityId2 + ", cityName3=" + cityName3
+						+ ", Location3=" + Location3 + ", cityId3=" + cityId3 + ", cityName4=" + cityName4
+						+ ", Location4=" + Location4 + ", cityId4=" + cityId4 + ", cityName5=" + cityName5
+						+ ", Location5=" + Location5 + ", cityId5=" + cityId5 + ", cityName6=" + cityName6
+						+ ", Location6=" + Location6 + ", cityId6=" + cityId6 + "]";
+			}
+
+		}
